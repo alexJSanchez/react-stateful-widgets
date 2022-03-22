35503d3d7a515315140512a5d768b98fd28e2090
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-22 10:39:38 -0400

    inputJS step3

--- src/components/Input.js
+++ src/components/Input.js
@@ -54,13 +54,13 @@ export default function Input() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color:(inputValue > 10) ? 'crimson':'royalblue'  , /* STEP 2 */
+    color:(inputValue > 10) ? 'crimson':'royalblue', /* STEP 2 */
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+      <div id='output' style={style}>{inputValue}</div> {/* STEP 3 */}
       <div>
         <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
